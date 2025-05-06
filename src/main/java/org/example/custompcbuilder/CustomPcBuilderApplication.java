bf26aafee84d12a4a1987e16de37fd0022f26301
s30175 <alex.biktimirov38@gmail.com>
2025-05-06 12:32:09 +0200

    Initial commit

---
+++ src/main/java/org/example/custompcbuilder/CustomPcBuilderApplication.java
@@ -0,0 +1,13 @@
+package org.example.custompcbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPcBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomPcBuilderApplication.class, args);
+    }
+
+}
